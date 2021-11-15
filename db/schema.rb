e31c96402c93528f503e36e5217869257ebd8f72
jules chevrant breton <jules.chevrant@hotmail.fr>
2021-11-15 18:06:28 +0100

    Modèle reservation créé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_161827) do
+ActiveRecord::Schema.define(version: 2021_11_15_170447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2021_11_15_161827) do
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "dates"
+    t.boolean "status"
+    t.bigint "user_id", null: false
+    t.bigint "ovni_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ovni_id"], name: "index_reservations_on_ovni_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +48,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_161827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "ovnis"
+  add_foreign_key "reservations", "users"
 end
