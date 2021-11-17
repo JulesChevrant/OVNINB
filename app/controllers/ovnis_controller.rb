1b2fee113b533eb1cb2d62d7ffe4a27e551cd862
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-11-17 13:04:35 +0100

    pundit & cloudinary

--- app/controllers/ovnis_controller.rb
+++ app/controllers/ovnis_controller.rb
@@ -1,5 +1,5 @@
 class OvnisController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show new]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @ovnis = policy_scope(Ovni)
@@ -15,17 +15,30 @@ class OvnisController < ApplicationController
     authorize @ovni
   end
 
-  def edit
-    @ovni = Ovni.find(params[:id])
+  def create
+    @ovni = Ovni.new(ovnis_params)
+    @ovni.user = current_user
+    authorize @ovni
+    if @ovni.save
+      redirect_to ovni_path(@ovni)
+    else
+      render :new
+    end
   end
 
-  def create
-
+  def edit
+    @ovni = Ovni.find(params[:id])
   end
 
   def destroy
     @ovni = Ovni.find(params[:id])
     @ovni.destroy
     redirect_to ovnis_path
+  end
+
+  private
+
+  def ovnis_params
+    params.require(:ovni).permit(:name, :photo, :price, :details)
   end
 end
---
+++ app/policies/ovni_policy.rb
@@ -0,0 +1,19 @@
+class OvniPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+end
